1aee54d58e45d900065e7306d2eca785dd6f6a7c
SeptAlfauzan <alfauzansepta@gmail.com>
2024-08-26 17:07:35 +0700

    add: introduction proof report progress widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:proofmaster/app/presentation/reports/student/introduction_proof/widgets/item.dart';
 import 'package:proofmaster/app/presentation/success_reset_pass/success_reset_pass.dart';
 import 'package:proofmaster/theme/text_theme.dart';
 import 'package:proofmaster/widgets/bottom_bar.dart';
@@ -14,15 +15,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Proof Master',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        textTheme: CustomTextTheme.proofMasterTextTheme,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SuccessResetPass(),
-    );
+        title: 'Proof Master',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          textTheme: CustomTextTheme.proofMasterTextTheme,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: WidgetPreview(
+            children: Column(
+          children: [
+            IntroductionProofItem(
+                text: "text",
+                finished: true,
+                finishedCount: 10,
+                totalCount: 10),
+            IntroductionProofItem(
+                text: "text",
+                finished: false,
+                finishedCount: 10,
+                totalCount: 10),
+          ],
+        )));
   }
 }
 
@@ -33,7 +47,7 @@ class WidgetPreview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Widget Preview"),
