60fd2020e3e046219b169b23448777534af86b69
SeptAlfauzan <alfauzansepta@gmail.com>
2024-12-03 11:59:52 +0700

    update(quiz): response model

--- lib/app/data/responses/student/get_quiz_questions_response/get_quiz_questions_response.dart
+++ lib/app/data/responses/student/get_quiz_questions_response/get_quiz_questions_response.dart
@@ -1,24 +1,29 @@
 import 'dart:convert';
 
 import 'package:equatable/equatable.dart';
+import 'package:proofmaster/app/data/responses/student/get_quiz_questions_response/prerequisite.dart';
 
 import 'datum.dart';
 
 class GetQuizQuestionsResponse extends Equatable {
   final List<Datum>? data;
+  final Prerequisite? prerequisite;
 
-  const GetQuizQuestionsResponse({this.data});
+  const GetQuizQuestionsResponse({this.data, this.prerequisite});
 
   factory GetQuizQuestionsResponse.fromMap(Map<String, dynamic> data) {
+    print(data);
     return GetQuizQuestionsResponse(
       data: (data['data'] as List<dynamic>?)
           ?.map((e) => Datum.fromMap(e as Map<String, dynamic>))
           .toList(),
+      // prerequisite: Prerequisite.fromMap(data['prerequisite']),
     );
   }
 
   Map<String, dynamic> toMap() => {
         'data': data?.map((e) => e.toMap()).toList(),
+        'prerequisite': prerequisite?.toMap()
       };
 
   /// `dart:convert`
@@ -34,12 +39,11 @@ class GetQuizQuestionsResponse extends Equatable {
   /// Converts [GetQuizQuestionsResponse] to a JSON string.
   String toJson() => json.encode(toMap());
 
-  GetQuizQuestionsResponse copyWith({
-    List<Datum>? data,
-  }) {
+  GetQuizQuestionsResponse copyWith(
+      {List<Datum>? data, Prerequisite? prerequisite}) {
     return GetQuizQuestionsResponse(
-      data: data ?? this.data,
-    );
+        data: data ?? this.data,
+        prerequisite: prerequisite ?? this.prerequisite);
   }
 
   @override
